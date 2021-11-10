86de9618ec09ae8a3f94b9f813d54c74730a615c
Wijitha Wijenayake <wijitha@calcey.com>
2021-11-10 11:36:57 +0530

    Added reactstrap component

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,18 +1,25 @@
 import React from "react";
-import { Button } from 'reactstrap';
-//import Button from 'react-bootstrap/Button'
+import { Button } from "reactstrap";
 
 import "./css-classes.scss";
 import styles from "./common.module.scss";
 import styles2 from "./common2.module.css";
 
 const AboutPage = () => {
+  const handleClick = () => {
+    console.log("Save button clicked");
+  };
+
   return (
     <>
       <div className="yellowText">This is about page</div>
       <div className={styles.redText}>This is page description</div>
       <div className={styles2.blueText}>This is page footer</div>
-      <div><Button color="danger">Save</Button> </div>
+      <div>
+        <Button color="danger" onClick={handleClick}>
+          Save
+        </Button>
+      </div>
     </>
   );
 };
